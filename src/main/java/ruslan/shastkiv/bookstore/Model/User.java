60d94bd074719304adabad752c29aaae273851c9
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-01 23:49:45 +0300

    Added all of required annotations for User model; Added lombok dependency;

--- src/main/java/ruslan/shastkiv/bookstore/Model/User.java
+++ src/main/java/ruslan/shastkiv/bookstore/Model/User.java
@@ -1,14 +1,29 @@
 package ruslan.shastkiv.bookstore.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.lang.NonNull;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String author;
+    @NonNull
+    @Column(unique = true)
     private String isbn;
+    @NonNull
     private BigDecimal price;
     private String description;
     private String coverImage;
-
 }
