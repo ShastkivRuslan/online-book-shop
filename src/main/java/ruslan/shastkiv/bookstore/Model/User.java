a14704331750d0c582a853c9f5a9eb1071d6cef1
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-01 23:26:08 +0300

    Created User model and add required fields without annotations;

---
+++ src/main/java/ruslan/shastkiv/bookstore/Model/User.java
@@ -0,0 +1,14 @@
+package ruslan.shastkiv.bookstore.Model;
+
+import java.math.BigDecimal;
+
+public class User {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
+}
