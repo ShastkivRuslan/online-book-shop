fb71528111a3ff6ca901ac295cc321d2c519bfa0
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-01 23:08:31 +0300

    initial settings

---
+++ src/test/java/ruslan/shastkiv/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.shastkiv.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
