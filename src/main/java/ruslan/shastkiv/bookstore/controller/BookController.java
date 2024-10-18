4597aa0aded0319e1de00f9bdc3eefcaa1599e1f
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-18 07:00:58 +0300

    Pagination and open-api; -added pagination support for the "Get All Books" and "Search Books" methods to improve performance and handle large datasets efficiently.  -integrated Swagger for API documentation and provided detailed descriptions for each method in the BookController, ensuring better API discoverability and understanding for developers. -mvn clean package -> ok;

--- src/main/java/ruslan/shastkiv/bookstore/controller/BookController.java
+++ src/main/java/ruslan/shastkiv/bookstore/controller/BookController.java
@@ -1,8 +1,10 @@
 package ruslan.shastkiv.bookstore.controller;
 
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,41 +20,73 @@ import ruslan.shastkiv.bookstore.dto.BookSearchParametersDto;
 import ruslan.shastkiv.bookstore.dto.CreateBookRequestDto;
 import ruslan.shastkiv.bookstore.service.BookService;
 
+@Tag(name = "Books", description = "Manage books in the bookstore")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
 
+    @Operation(
+            summary = "Get all books",
+            description = "Retrieve a list of all books. Supports pagination."
+    )
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.getAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.getAll(pageable);
     }
 
+    @Operation(
+            summary = "Get book by ID",
+            description = "Retrieve a specific book by its ID. "
+                    + "Returns 404 if the book is not found."
+    )
     @GetMapping("/{id}")
     public BookDto getBokById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
+    @Operation(
+            summary = "Create a new book",
+            description = "Add a new book to the bookstore. "
+                    + "The request body must include title, author, ISBN, and price."
+    )
     @PostMapping
-    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.createBook(bookDto);
     }
 
+    @Operation(
+            summary = "Update book",
+            description = "Update an existing book. "
+                    + "Requires the book ID and the updated details in the request body."
+    )
     @PutMapping("/{id}")
-    public BookDto updateBook(@PathVariable Long id,
-                              @RequestBody @Valid CreateBookRequestDto requestDto) {
+    public BookDto updateBook(@PathVariable Long id, @RequestBody CreateBookRequestDto requestDto) {
         return bookService.updateBook(id, requestDto);
     }
 
+    @Operation(
+            summary = "Delete a book",
+            description = "Remove a book from the bookstore by its ID. "
+                    + "Returns 404 if the book is not found."
+    )
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
 
+    @Operation(
+            summary = "Search for books",
+            description = "Search for books by author, title, or price range. "
+                    + "If only one price is provided, it will be treated as the minimum price, "
+                    + "and results will be returned starting from that price. "
+                    + "If two prices are provided, they will be treated as "
+                    + "the minimum and maximum price range.")
+
     @GetMapping("/search")
-    public List<BookDto> search(@Valid BookSearchParametersDto searchParametersDto) {
-        return bookService.search(searchParametersDto);
+    public List<BookDto> search(BookSearchParametersDto searchParametersDto, Pageable pageable) {
+        return bookService.search(searchParametersDto, pageable);
     }
 }
