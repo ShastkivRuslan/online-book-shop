c593dffb03e5cdc02374f6d7e511efb30b379184
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-29 21:55:36 +0200

    mvn clean package -> passed;

--- src/main/java/ruslan/shastkiv/bookstore/model/User.java
+++ src/main/java/ruslan/shastkiv/bookstore/model/User.java
@@ -1,6 +1,11 @@
 package ruslan.shastkiv.bookstore.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,7 +25,7 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private String first_name;
+    private String firstName;
 
     @Column(nullable = false)
     private String lastName;
