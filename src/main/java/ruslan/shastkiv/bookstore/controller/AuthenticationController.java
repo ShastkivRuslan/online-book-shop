0752059a7c3206a0598bc87dda3de22bce9b60c0
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-10-30 16:04:29 +0200

    -added swagger documentation;

--- src/main/java/ruslan/shastkiv/bookstore/controller/AuthenticationController.java
+++ src/main/java/ruslan/shastkiv/bookstore/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package ruslan.shastkiv.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +13,17 @@ import ruslan.shastkiv.bookstore.dto.user.UserRegistrationRequestDto;
 import ruslan.shastkiv.bookstore.exception.RegistrationException;
 import ruslan.shastkiv.bookstore.service.user.UserService;
 
+@Tag(name = "Authentication",
+        description = "Endpoints for managing user authentication and registration")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
 
+    @Operation(summary = "Register a new user",
+            description = "Registers a new user with provided credentials. "
+                    + "Ensures password validation and stores user data in the system.")
     @PostMapping("/registration")
     public UserDto register(@RequestBody @Valid UserRegistrationRequestDto registrationRequestDto)
             throws RegistrationException {
