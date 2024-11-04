391d939b1b4ad475331b4fb7c0a7eb4b57a02b27
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-04 22:19:16 +0200

    -added findByEmail method to UserRepository;

--- src/main/java/ruslan/shastkiv/bookstore/repository/user/UserRepository.java
+++ src/main/java/ruslan/shastkiv/bookstore/repository/user/UserRepository.java
@@ -1,8 +1,11 @@
 package ruslan.shastkiv.bookstore.repository.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ruslan.shastkiv.bookstore.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
