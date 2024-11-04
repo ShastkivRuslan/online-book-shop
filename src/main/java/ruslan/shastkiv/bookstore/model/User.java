6a5f1b264f428c6ad2be7c9bf0d45bd890a123bb
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-04 22:20:12 +0200

    -refactored imports; mvn clean package -> done;

--- src/main/java/ruslan/shastkiv/bookstore/model/User.java
+++ src/main/java/ruslan/shastkiv/bookstore/model/User.java
@@ -6,13 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Getter
 @Setter
