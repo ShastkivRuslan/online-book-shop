d3c7f55a669d17f510030df2322c1ad764d62705
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-04 22:22:03 +0200

    -implemented UserDetailsService in UserDetailsServiceImpl.class;

---
+++ src/main/java/ruslan/shastkiv/bookstore/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package ruslan.shastkiv.bookstore.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ruslan.shastkiv.bookstore.repository.user.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("Can`t find user by email ["
+                        + email + "]")
+        );
+    }
+}
