8525b2fbda1227dcb7b8418c2215271dde584e31
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-11 17:17:15 +0200

    Created category entity with required fields;

---
+++ src/main/java/ruslan/shastkiv/bookstore/model/Category.java
@@ -0,0 +1,18 @@
+package ruslan.shastkiv.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
