33312994f0732e32d5feb781ba2e36b6a3c718c6
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-12 12:59:52 +0200

    added hibernate validation to CategoryRequestDto;

--- src/main/java/ruslan/shastkiv/bookstore/dto/category/CategoryRequestDto.java
+++ src/main/java/ruslan/shastkiv/bookstore/dto/category/CategoryRequestDto.java
@@ -1,6 +1,11 @@
 package ruslan.shastkiv.bookstore.dto.category;
 
-public record CategoryRequestDto(String name,
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record CategoryRequestDto(@Length(max = 20)
+                                 @NotBlank
+                                 String name,
+                                 @Length(max = 1000)
                                  String description) {
 }
-//todo: add validations for all fields
