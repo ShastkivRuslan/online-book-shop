92fe027595801820c27b93be0436f1d3632930fd
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-11-28 15:05:46 +0200

    -build fix;

--- src/test/java/ruslan/shastkiv/bookstore/service/CategoryServiceTest.java
+++ src/test/java/ruslan/shastkiv/bookstore/service/CategoryServiceTest.java
@@ -1,7 +1,7 @@
 package ruslan.shastkiv.bookstore.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
